0788cb1b9d2a4f6c3ebe2c8588ff663fcca04d80
callum <callumcancreate@gmail.com>
2020-06-04 14:14:07 +1000

    add ci

---
+++ deploy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables
+BUILD_PATH=
+BRANCH=
+
+# Test
+TEST_BUILD_PATH="./testbuild"
+TEST_BRANCH="development"
+
+# Prod
+PROD_BUILD_PATH="./prodbuild"
+PROD_BRANCH="master"
+
+
+# Parse options
+while getopts ":htp" opt; do
+  case ${opt} in
+    t) 
+      echo "Deploying test"
+      $BUILD_PATH = $TEST_BUILD_PATH
+      $BRANCH = $TEST_BRANCH
+      ;;
+    p) 
+      echo "Deploying produciton"
+      $BUILD_PATH = $PROD_BUILD_PATH
+      $BRANCH = $PROD_BRANCH
+      ;;
+    \?) 
+      echo "Must pass a deployment option"
+      echo "Usage:"
+      echo "  -t  deploy test"
+      echo "  -p  deploy production"
+      exit 1
+      ;;
+  esac
+done
+
+git stash
+git pull
+git checkout $BRANCH
+npm install
+npm run build
+if [[ -d $BUILD_PATH ]]
+then
+    rm -R $BUILD_PATH
+fi
+cp -R build/ $BUILD_PATH
+chmod u+x $BUILD_PATH/

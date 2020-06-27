1188f0ba781da3c7f6217c15c08b0184c8957a8f
callum <callumcancreate@gmail.com>
2020-06-27 14:01:18 +1000

    wip

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,28 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 import LoginForm from "../components/forms/LoginForm";
+import Card from "../components/Card";
 
 const LoginPage = () => {
-  const { user } = useContext(AuthContext);
-  const location = useLocation();
-  const referredBy =
-    location?.search &&
-    qs.parse(location.search, { ignoreQueryPrefix: true })?.referredBy;
-  if (user && !referredBy) return <Redirect to="/" />;
   return (
     <Layout className="login-page" navLinks={[]}>
       <div className="d-flex justify-content-center container py-5 align-items-center">
         <Card className="card-container">
-          <div className="row">
-            {!referredBy && (
-              <div className="col-sm">
-                <LoginForm />
-              </div>
-            )}
-            <div className="col-sm">
-              <RegisterForm initialValues={{ referredBy }} />
-            </div>
-          </div>
+          <LoginForm />
         </Card>
       </div>
     </Layout>

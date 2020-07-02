9cd2f2e587b2bac50130b453d896253f14de74c3
Callum Gibson <46746438+callumcancreate@users.noreply.github.com>
2020-07-02 15:42:37 +1000

    add alert to add address modal (#12)

--- src/components/forms/AddressForm/AddressForm.js
+++ src/components/forms/AddressForm/AddressForm.js
@@ -4,6 +4,7 @@ import validate from "./validate";
 import Input from "../form-inputs/Input";
 import SubmitSpinnerButton from "../SubmitSpinnerButton";
 import ErrorMessage from "../../ErrorMessage";
+import { Alert } from "react-bootstrap";
 
 const defaultInitialValues = {
   percent: 100,
@@ -23,6 +24,16 @@ const AddressForm = ({
     <Formik initialValues={iv} validate={validate} onSubmit={onSubmit}>
       {({ isSubmitting, errors }) => (
         <Form>
+          <Alert variant="primary" className="mb-4">
+            <b className="alert-heading">
+              Your payment can be sent to multiple bitcoin addresses.
+            </b>
+            <span className="ml-2">
+              For example, you may want to split your payment and send 50% to a
+              cold storage wallet and 50% to a hot wallet. Please set the
+              percentage required in the below field or leave at 100.
+            </span>
+          </Alert>
           <Input name="percent" label="Percent" />
           <Input name="label" label="Label" />
           <Input name="address1" label="Address" />

6f34ac59d3f96736b571e63d1a1e5a9839d48c22
Callum Gibson <46746438+callumcancreate@users.noreply.github.com>
2020-08-06 16:16:16 +1000

    Gpib 203 (#28)
    * wip
    
    * setup boilerplate code
    
    * add api call to email instructions
    
    * clean up code
    
    * wip
    
    * wip
    
    * add email employer button

--- src/components/PayInformationActions.js
+++ src/components/PayInformationActions.js
@@ -1,7 +1,9 @@
 import React, { useReducer, useContext } from "react";
+import { isEmail } from "validator";
 import SubmitSpinnerButton from "./forms/SubmitSpinnerButton";
 import { AuthContext } from "./Auth";
 import gpib from "../apis/gpib";
+import ButtonInputSwitcher from "./ButtonInputSwitcher";
 
 const icons = {
   email: "mail-outline",
@@ -15,15 +17,15 @@ const messages = {
   email: {
     default: "Email Pay Instructions to me",
     error: "Something went wrong. Try emailing instructions again?"
-  }
+  },
   // sms: {
   //   default: "SMS Pay Instructions to me",
   //   error: "Something went wrong. Try sending instructions by SMS again?"
   // },
-  // customEmail: {
-  //   default: "Email Pay Instructions to another address",
-  //   error: "Something went wrong. Try emailing instructions again?"
-  // }
+  customEmail: {
+    default: "Email Pay Instructions to another address",
+    error: "Something went wrong. Try emailing instructions again?"
+  }
 };
 
 const reducer = (state, action) => {
@@ -31,6 +33,7 @@ const reducer = (state, action) => {
   const newState = { ...state };
   switch (type) {
     case "BEGIN":
+      newState[target].message = messages[target].default;
       newState[target].isSending = true;
       newState[target].error = null;
       newState[target].isSent = false;
@@ -47,6 +50,13 @@ const reducer = (state, action) => {
       newState[target].icon = icons.error;
       newState[target].message = messages[target].error;
       return newState;
+    case "RESET":
+      newState[target].isSending = false;
+      newState[target].isSent = false;
+      newState[target].error = null;
+      newState[target].icon = icons[target];
+      newState[target].message = messages[target].default;
+      return newState;
     default:
     //  no default
   }
@@ -59,21 +69,21 @@ const initialState = {
     error: null,
     icon: icons.email,
     message: messages.email.default
-  }
+  },
   // sms: {
   //   isSent: false,
   //   isSending: false,
   //   error: null,
   //   icon: icons.sms,
   //   message: messages.sms.default
-  // },
-  // customEmail: {
-  //   isSent: false,
-  //   isSending: false,
-  //   error: null,
-  //   icon: icons.customEmail,
-  //   message: messages.customEmail.default
-  // }
+  // },SubmitSpinnerButton
+  customEmail: {
+    isSent: false,
+    isSending: false,
+    error: null,
+    icon: icons.customEmail,
+    message: messages.customEmail.default
+  }
 };
 
 const actionSharedProps = {
@@ -87,15 +97,29 @@ const PayInformationActions = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { user } = useContext(AuthContext);
 
-  const emailUserInstructions = async () => {
+  const emailInstructionsToMe = async () => {
+    await emailInstructions(user?.email, "email");
+  };
+
+  const emailInstructionsToOther = async (customEmail, actions) => {
+    if (!isEmail(customEmail))
+      return dispatch({
+        target: "customEmail",
+        type: "ERROR",
+        error: "Invalid email"
+      });
+    actions.setInputShown(false);
+
+    await emailInstructions(customEmail, "customEmail");
+  };
+
+  const emailInstructions = async (email, target) => {
     try {
-      dispatch({ target: "email", type: "BEGIN" });
-      await gpib.secure.get(
-        `/email/payinstructions/${user.id}?email=${user?.email}`
-      );
-      dispatch({ target: "email", type: "DONE" });
+      dispatch({ target, type: "BEGIN" });
+      await gpib.secure.get(`/email/payinstructions/${user.id}?email=${email}`);
+      dispatch({ target, type: "DONE" });
     } catch (error) {
-      dispatch({ target: "email", type: "ERROR", error });
+      dispatch({ target, type: "ERROR", error });
     }
   };
 
@@ -105,7 +129,17 @@ const PayInformationActions = () => {
         icon={state.email.icon}
         submitText={state.email.message}
         isSubmitting={state.email.isSending}
-        onClick={emailUserInstructions}
+        onClick={emailInstructionsToMe}
+        {...actionSharedProps}
+      />
+      <br />
+      <ButtonInputSwitcher
+        icon={state.customEmail.icon}
+        submitText={state.customEmail.message}
+        isSubmitting={state.customEmail.isSending}
+        onSubmit={emailInstructionsToOther}
+        confirmText="Send"
+        onCancel={() => dispatch({ type: "RESET", target: "customEmail" })}
         {...actionSharedProps}
       />
     </div>

30753f892f375ae9583f325ea3f495abb01e8904
callum <callumcancreate@gmail.com>
2020-09-25 14:11:39 +1000

    add settings card to profile page

---
+++ src/components/forms/form-inputs/ToggleField.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useField } from "formik";
+
+const ToggleField = ({ label, notInline, className, ...props }) => {
+  const [field, meta, helpers] = useField(props);
+  const toggleValue = () => helpers.setValue(!meta.value);
+  let classes = "toggle-field form-group";
+  if (className) classes += ` ${className}`;
+  if (!notInline) classes += " inline";
+  return (
+    <div className={classes}>
+      {label && <label>{label}</label>}
+      <div
+        className={`switch ${meta.value ? "checked" : ""}`}
+        onClick={toggleValue}
+      >
+        <input type="checkbox" {...props} {...field} />
+        <span className="slider" />
+      </div>
+    </div>
+  );
+};
+
+export default ToggleField;

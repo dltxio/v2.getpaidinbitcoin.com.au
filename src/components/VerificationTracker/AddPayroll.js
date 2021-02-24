86fe3fc22cf2979beaf5a9ecd63b4ff7ff5af5ab
Alex Xiang <xiangzhe0812@gmail.com>
2021-02-24 12:54:57 +1000

    GPIB-356

--- src/components/VerificationTracker/AddPayroll.js
+++ src/components/VerificationTracker/AddPayroll.js
@@ -1,10 +1,12 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import { mutate } from "swr";
 import DepositHintsForm from "components/deposit-hints/DepositHintsForm";
 import gpib from "apis/gpib";
 import { AuthContext } from "components/auth/Auth";
+import { Alert } from "react-bootstrap";
 
 const AddPayroll = ({ userEnterprise }) => {
+  const [message, setMessage] = useState();
   const initialValues = useMemo(
     () => ({ employerName: userEnterprise?.name }),
     [userEnterprise]
@@ -21,6 +23,10 @@ const AddPayroll = ({ userEnterprise }) => {
         await gpib.secure.get(
           `/email/payinstructions/${user.id}?email=${userEnterprise.contactEmail}`
         );
+        setMessage(
+          "Your company will be automatically emailed with the updated details, there is no action required from you "
+        );
+        await new Promise((resolve) => setTimeout(resolve, 5000));
       }
       await mutate(`/user/${user.id}/deposithints`);
       actions.setSubmitting(false);
@@ -35,6 +41,7 @@ const AddPayroll = ({ userEnterprise }) => {
       <p>
         <b>Add your payroll information.</b>
       </p>
+      {message && <Alert variant="success">{message}</Alert>}
       <div>
         <DepositHintsForm
           onSubmit={updatePayroll}

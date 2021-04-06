b0f1f3d3d3d6658b01349436242ce3c1bb198cd2
Alex Xiang <xiangzhe0812@gmail.com>
2021-04-06 23:12:41 +1000

    skip kyc

--- src/components/VerificationTracker/VerifyMobile.js
+++ src/components/VerificationTracker/VerifyMobile.js
@@ -9,7 +9,7 @@ import { AuthContext } from "components/auth/Auth";
 
 const VerifyEmail = () => {
   const [hasSent, setSent] = useState(false);
-  const { user } = useContext(AuthContext);
+  const { user, setVerified } = useContext(AuthContext);
   const [message, setMessage] = useState();
 
   const sendSMS = async (values, actions) => {
@@ -74,29 +74,39 @@ const VerifyEmail = () => {
     );
   };
 
+  const skipKYC = () => {
+    setVerified(true);
+  };
   return (
     <div>
-      {message && <Alert variant="success">{message}</Alert>}
-      <p>
-        <b>Verify your mobile to continue.</b>
-      </p>
-      <SingleInputForm
-        submitText="Send verification code"
-        placeholder="Please enter your mobile number"
-        onSubmit={sendSMS}
-        name="mobile"
-        validate={validateMobile}
-        style={{ display: hasSent ? "none" : undefined }}
-      />
-      <SingleInputForm
-        onSubmit={verifyCode}
-        placeholder="Please enter your 6-digit verification code"
-        submitText="Verify code"
-        name="code"
-        validate={validateCode}
-        style={{ display: hasSent ? undefined : "none" }}
-        renderActions={renderCodeActions}
-      />
+      <div>
+        {message && <Alert variant="success">{message}</Alert>}
+        <p>
+          <b>Verify your mobile to continue.</b>
+        </p>
+        <SingleInputForm
+          submitText="Send verification code"
+          placeholder="Please enter your mobile number"
+          onSubmit={sendSMS}
+          name="mobile"
+          validate={validateMobile}
+          style={{ display: hasSent ? "none" : undefined }}
+        />
+        <SingleInputForm
+          onSubmit={verifyCode}
+          placeholder="Please enter your 6-digit verification code"
+          submitText="Verify code"
+          name="code"
+          validate={validateCode}
+          style={{ display: hasSent ? undefined : "none" }}
+          renderActions={renderCodeActions}
+        />
+      </div>
+      <div className="mt-5 d-flex flex-row-reverse">
+        <Button variant="primary" onClick={skipKYC}>
+          Skip KYC
+        </Button>
+      </div>
     </div>
   );
 };

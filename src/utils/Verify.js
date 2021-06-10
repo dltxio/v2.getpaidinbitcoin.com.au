eb69ca1ed41718a880de8e7630bd33f10d6bf094
Alex Xiang <alex.xiang@dltx.io>
2021-06-10 10:49:37 +1000

    GPIB-422 (#113)
    * digital Id
    
    * center the button
    
    * remove user info form and change verify.js to function component
    
    * aus post
    
    * setup example env, fix small issue

---
+++ src/utils/Verify.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { mutate } from "swr";
+import gpib from "../apis/gpib";
+
+const Verify = ({ setIdVerificationStatus, statuses, user }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = `${process.env.REACT_APP_DIGITAL_SOURCE}`
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      /* Verify with Digital iD */
+      window.digitalId.init({
+        clientId: `${process.env.REACT_APP_DIGITAL_ID_CLIENT_ID}`,
+        uxMode: "popup",
+        onComplete: async function (msg) {
+          if (msg.error) {
+            if (msg.error === 'verification_cancelled') {
+              // TODO cancelled logic
+              setIdVerificationStatus(statuses.CANCELLED);
+            }
+            return;
+          }
+          if (msg && msg.code !== "" && msg.transaction_id !== "") {
+            var response = await gpib.secure.post("/user/digitalId", {
+              code: msg.code,
+              transactionID: msg.transaction_id
+            });
+            if (response.status === 200) {
+              setIdVerificationStatus(statuses.VERIFIED);
+
+            }
+            if (response.status === 400) {
+              setIdVerificationStatus(statuses.REJECTED);
+            }
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+            await mutate(user && `/user/${user.id}`);
+          }
+        }
+      });
+    };
+  }, [setIdVerificationStatus, statuses, user]);
+  return (
+    <div id="digitalid-verify" className="d-flex justify-content-center"></div>
+  );
+};
+export default Verify;

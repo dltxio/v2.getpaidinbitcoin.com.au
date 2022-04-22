939e750d3820bee795eb38124d6e365f8c633652
Alex.Xiang <alex.xiang@dltx.io>
2022-04-22 15:23:53 +1000

    wording  change and add $

--- src/components/referral/ReferralTable.js
+++ src/components/referral/ReferralTable.js
@@ -2,19 +2,20 @@ import React from "react";
 import Table from "components/Table";
 import truncate from "../../utils/truncate";
 import moment from "moment";
+import { format as format$ } from "currency-formatter";
 
 const columnConfig = {
   fixedAmount: {
     children: "Amount",
     width: "15%",
-    dataFormat: (amt) => truncate(amt, 8)
+    dataFormat: (amt) => format$(truncate(amt, 8), { code: "AUD" })
   },
   firstName: {
     children: "First Name",
     width: "15%"
   },
   created: {
-    children: "Created",
+    children: "Referral Date",
     width: "15%",
     dataFormat: (cell) => cell && moment(cell).format("DD-MM-YYYY")
   }

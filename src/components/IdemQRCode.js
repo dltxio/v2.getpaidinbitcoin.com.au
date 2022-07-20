f736df7857ef5db62a05c05433aa7f291693d51a
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2022-07-21 09:20:47 +1000

    IDEM-100 (#146)
    * feat: make idem page
    
    * UI changes
    
    * use d flex
    
    * Added UI styling changes
    
    * Added cards and QR code
    
    * finish call backend and remove unused stuff
    
    * removed extra divs
    
    Co-authored-by: JasminFarhi <jasmin.farhi@dltx.io>
    Co-authored-by: Alex Xiang <alex.xiang@dltx.io>

---
+++ src/components/IdemQRCode.js
@@ -0,0 +1,8 @@
+import QRCode from "qrcode.react";
+import React from "react";
+
+const IdemQRCode = () => {
+  return <QRCode id="IdemQRCode" value="idem://exchange/vendors/1" />;
+};
+
+export default IdemQRCode;
--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -4,11 +4,12 @@ import { isEmail } from "validator";
 import Input from "components/forms/Input";
 import SubmitSpinnerButton from "components/forms/SubmitSpinnerButton";
 import gpib from "apis/gpib";
+import Card from "components/Card";
 import ErrorMessage from "components/ErrorMessage";
 import { AuthContext } from "components/auth/Auth";
 import { minPasswordLength } from "constants/index";
 import { useHistory } from "react-router-dom";
-import "./RegisterForm.scss"
+import "./RegisterForm.scss";
 
 const defaultValues = {
   email: "",
@@ -52,7 +53,12 @@ const validate = ({ email, password, passwordMatch, firstName, lastName }) => {
   return errors;
 };
 
-const RegisterForm = ({ initialValues: _iv, lockReferralCode, enterprise, logo }) => {
+const RegisterForm = ({
+  initialValues: _iv,
+  lockReferralCode,
+  enterprise,
+  logo
+}) => {
   const initialValues = { ...defaultValues, ..._iv };
   const { login } = useContext(AuthContext);
   const history = useHistory();
@@ -73,50 +79,62 @@ const RegisterForm = ({ initialValues: _iv, lockReferralCode, enterprise, logo }
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validate={validate}
-      onSubmit={onSubmit}
-    >
-      {({ isSubmitting, errors }) => (
-        <Form style={{ flex: 1, width: "100%" }}>
-          <div className="">
-            {logo && (<div className="mb-5 mt-2 d-flex justify-content-center"><img src={`${process.env.REACT_APP_API_URL}/Logos/${logo}`} alt="logo" className="logo-image" /></div>)}
-          </div>
-          <Input
-            name="email"
-            placeholder="Please register with your own email"
-            disabled={initialValues?.email}
-          />
-          <Input name="password" type="password" placeholder="Password" />
-          <Input
-            name="passwordMatch"
-            type="password"
-            placeholder="Confirm Password"
-          />
-          <Input
-            name="firstName"
-            placeholder="First Name"
-            disabled={initialValues?.firstName}
-          />
-          <Input
-            name="lastName"
-            placeholder="Last Name"
-            disabled={initialValues?.lastName}
-          />
-          {!enterprise && (
-            <Input
-              name="referralCode"
-              placeholder="Offer or Referral Code"
-              disabled={lockReferralCode}
-            />
-          )}
+    <Card style={{ width: 500 }}>
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting, errors }) => (
+          <Form>
+            {logo && (
+              <div className="mb-5 mt-2 flex justify-content-center">
+                <img
+                  src={`${process.env.REACT_APP_API_URL}/Logos/${logo}`}
+                  alt="logo"
+                  className="logo-image"
+                />
+              </div>
+            )}
 
-          <ErrorMessage error={errors.hidden} />
-          <SubmitSpinnerButton submitText="Join" isSubmitting={isSubmitting} />
-        </Form>
-      )}
-    </Formik>
+            <Input
+              name="email"
+              placeholder="Please register with your own email"
+              disabled={initialValues?.email}
+            />
+            <Input name="password" type="password" placeholder="Password" />
+            <Input
+              name="passwordMatch"
+              type="password"
+              placeholder="Confirm Password"
+            />
+            <Input
+              name="firstName"
+              placeholder="First Name"
+              disabled={initialValues?.firstName}
+            />
+            <Input
+              name="lastName"
+              placeholder="Last Name"
+              disabled={initialValues?.lastName}
+            />
+            {!enterprise && (
+              <Input
+                name="referralCode"
+                placeholder="Offer or Referral Code"
+                disabled={lockReferralCode}
+              />
+            )}
+
+            <ErrorMessage error={errors.hidden} />
+            <SubmitSpinnerButton
+              submitText="Join"
+              isSubmitting={isSubmitting}
+            />
+          </Form>
+        )}
+      </Formik>
+    </Card>
   );
 };
 
